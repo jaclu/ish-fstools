226d777bc51ba3bfeaaace74f7af745037c43107
Jacob Lundqvist <Jacob.Lundqvist@gmail.com>
2025-12-18 15:40:51 +0100

    improved post build cleanup

--- tools/cleanup_build_env.sh
+++ tools/cleanup_build_env.sh
@@ -7,15 +7,19 @@
 #  Copyright (c) 2022-2025: Jacob.Lundqvist@gmail.com
 #
 #  Cleanup build env
-#  ansible can't be run on iSH anyhow and it consumes several hundred MB
-#  so no point in keeping it
 #
-
-rm -rf /root/ish-fstools
-rm -rf /iCloud/*
 
 #
 #  ansible can't be run on iSH so no point in keeping it around.
 #  AND it consumes over 450 MB and 46k files...
 #
 apk del ansible
+
+#
+# remove stuff used during build of FS
+#
+rm -rf /iCloud/*
+rm -f  /tmp/ssh_conf.tgz
+rm -rf /tmp/ansible_facts_cache
+rm -rf /root/.ansible
+rm -rf /root/ish-fstools
