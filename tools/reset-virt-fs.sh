94a2718b554750b9a737bf81a943f052e5d7bc71
jaclu <jacob.lundqvist@gmail.com>
2026-01-15 16:26:24 +0100

    linting

--- tools/reset-virt-fs.sh
+++ tools/reset-virt-fs.sh
@@ -234,7 +234,7 @@ load_utils() {
     _lu_d_base="${1:-$d_repo}"
     _lu_f_utils="$_lu_d_base"/tools/script-utils.sh
 
-    # shellcheck source=tools/script-utils.sh disable=SC2317
+    # shellcheck source=tools/script-utils.sh disable=SC1091,SC2317
     . "$_lu_f_utils" || {
         printf '\nERROR: Failed to source: %s\n' "$_lu_f_utils" >&2
         exit 1
