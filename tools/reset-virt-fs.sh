12cc68c1b8dac3620b965434d90d4b610a5f2c18
Jacob Lundqvist <Jacob.Lundqvist@gmail.com>
2026-01-17 23:08:33 +0100

    changed chroot history

--- tools/reset-virt-fs.sh
+++ tools/reset-virt-fs.sh
@@ -240,10 +240,10 @@ prepare_shell_env() {
         echo "/root/ish-fstools/tools/cleanup_build_env.sh"
         echo "time $cmd_2"
         echo "time $cmd_1"
+        echo "time $cmd_1 c"
         # echo "time $cmd_1 && time $cmd_2"
-        # echo "time $cmd_1 c"
-        echo "time $cmd_2 q"
-        echo "time $cmd_1 q"
+        # echo "time $cmd_2 q"
+        # echo "time $cmd_1 q"
         # s="[ -f /etc/alpine-release ] && apk add bash"
         # echo "$s ; ./ish-fstools/tools/fs_cleanup.sh"
         # echo ./ish-fstools/tools/fs_cleanup.sh
